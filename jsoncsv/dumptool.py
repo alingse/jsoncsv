bb7a1ff35f042ad45b4017200ac74c3c3498e86d
alingse <alingse@foxmail.com>
2017-05-10 00:22:45 +0800

    整理 mkexcel 的实现，尝试清理一下
    Too many class --

--- jsoncsv/dumptool.py
+++ jsoncsv/dumptool.py
@@ -11,9 +11,6 @@ class Dump(object):
     def __init__(self, fin, fout, **kwargs):
         self.fin = fin
         self.fout = fout
-        self.datas = []
-        self.headers = set()
-
         self.initialize(**kwargs)
 
     def initialize(self, **kwargs):
@@ -31,46 +28,80 @@ class Dump(object):
 
         return value
 
-    def load_headers(self, read_row=None):
-        i = 0
-        for line in self.fin:
-            obj = json.loads(line)
-            map(self.headers.add, obj.iterkeys())
-            self.datas.append(obj)
-            if read_row is not None and i >= read_row:
-                break
+    def prepare(self):
+        pass
 
-    def write_headers(self):
-        raise NotImplementedError
-
-    def write_obj(self, obj):
+    def dump_file(self, obj):
         raise NotImplementedError
 
     def on_finish(self):
         pass
 
+    def dump(self):
+        self.prepare()
+        self.dump_file()
+        self.on_finish()
+
+
+class ReadHeadersMixin(object):
+
+    def load_headers(fin, read_row=None):
+        headers = set()
+        datas = []
+
+        if not read_row or read_row < 1:
+            read_row = 1
+
+        for line in fin:
+            obj = json.loads(line)
+
+            headers.update(obj.iterkeys())
+            datas.append(obj)
+
+            read_row -= 1
+            if not read_row:
+                break
+
+        return (headers, datas)
+
+
+class DumpExcel(Dump, ReadHeadersMixin):
+
+    def initialize(self, **kwargs):
+        super(DumpExcel, self).initialize(**kwargs)
+        self.read_row = kwargs.get('read_row')
+
+    def prepare(self):
+        headers, datas = self.load_headers(self.fin, self.read_row)
+        self.headers = headers
+        self.datas = datas
+
+    def write_headers(self):
+        raise NotImplementedError
+
+    def write_obj(self):
+        raise NotImplementedError
+
     def dump_file(self):
-        if self.datas:
-            map(self.write_obj, self.datas)
+        self.write_headers()
+
+        for obj in self.datas:
+            self.write_obj(obj)
 
         for line in self.fin:
             obj = json.loads(line)
             self.write_obj(obj)
 
-    def dump(self):
-        self.write_headers()
-        self.dump_file()
-        self.on_finish()
 
+class DumpCSV(DumpExcel):
 
-class DumpCSV(Dump):
+    def initialize(self, **kwargs):
+        super(DumpCSV, self).initialize(**kwargs)
 
-    def initialize(self, separator=','):
-        self.separator = separator
+        self.separator = kwargs.get('separator', ',')
 
     def write_headers(self):
-        self.headers = list(self.headers)
-        content = self.separator.join(self.headers)
+        content = self.separator.join(list(self.headers))
         self.fout.write(content)
         self.fout.write('\n')
 
@@ -79,48 +110,53 @@ class DumpCSV(Dump):
         return str(value)
 
     def write_obj(self, obj):
-        values = [obj.get(head) for head in self.headers]
-        values = map(self.patch, values)
+        values = [
+            self.patch(obj.get(head))
+            for head in self.headers
+        ]
         content = self.separator.join(values)
         self.fout.write(content)
         self.fout.write('\n')
 
 
-class DumpExcel(Dump):
+class DumpXLS(DumpExcel):
 
     def initialize(self, **kwargs):
+        super(DumpCSV, self).initialize(**kwargs)
+
+        self.sheet = kwargs.get('sheet', 'Sheet1')
         self.wb = xlwt.Workbook(encoding='utf-8')
-        self.ws = self.wb.add_sheet('Sheet1')
+        self.ws = self.wb.add_sheet(self.sheet)
         self.row = 0
         self.cloumn = 0
 
     def write_headers(self):
-        self.headers = list(self.headers)
-
         for head in self.headers:
             self.ws.write(self.row, self.cloumn, head)
             self.cloumn += 1
+        self.row += 1
 
     def write_obj(self, obj):
-        values = [obj.get(head) for head in self.headers]
-        values = map(self.patch, values)
+        values = [
+            self.patch(obj.get(head))
+            for head in self.headers
+        ]
 
-        self.row += 1
         self.cloumn = 0
         for value in values:
             self.ws.write(self.row, self.cloumn, value)
             self.cloumn += 1
+        self.row += 1
 
     def on_finish(self):
         self.wb.save(self.fout)
 
 
-def dumpfile(fin, fout, type_, read_row=None):
+def dumpexcel(fin, fout, type_, **kwargs):
     if type_ == 'csv':
         DumpKlass = DumpCSV
     elif type_ == 'xls':
-        DumpKlass = DumpExcel
+        DumpKlass = DumpXLS
 
-    dump = DumpKlass(fin, fout)
-    dump.load_headers(read_row)
+    dump = DumpKlass(fin, fout, **kwargs)
     dump.dump()
