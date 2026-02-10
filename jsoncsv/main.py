562fdd9673272034162f087e1da5622205908647
alingse <alingse@foxmail.com>
2026-02-10 23:17:57 +0800

    Format code with ruff
    Apply ruff formatting to standardize code style across the project.

--- jsoncsv/main.py
+++ jsoncsv/main.py
@@ -15,40 +15,22 @@ from jsoncsv.utils import unit_char
 
 def separator_type(sep: str) -> str:
     if len(sep) != 1:
-        raise click.BadOptionUsage(option_name='separator',
-                                   message='separator can only be a char')
+        raise click.BadOptionUsage(option_name="separator", message="separator can only be a char")
     if sep == unit_char:
-        raise click.BadOptionUsage(option_name='separator',
-                                   message='separator can not be `\\` ')
+        raise click.BadOptionUsage(option_name="separator", message="separator can not be `\\` ")
     return sep
 
 
 @click.command()
-@click.option('-A',
-              '--array',
-              'json_array',
-              is_flag=True,
-              default=False,
-              help='read input file as json array')
-@click.option('-s',
-              '--sep',
-              'separator',
-              type=separator_type,
-              default='.',
-              help='separator')
-@click.option('--safe', is_flag=True, help='use safe mode')
-@click.option('-r',
-              '--restore',
-              'restore',
-              is_flag=True,
-              help='restore expanded json')
-@click.option('-e',
-              '--expand',
-              'expand',
-              is_flag=True,
-              help='expand json (default True)')
-@click.argument('input', type=click.File('r', encoding='utf-8'), default='-')
-@click.argument('output', type=click.File('w', encoding='utf-8'), default='-')
+@click.option(
+    "-A", "--array", "json_array", is_flag=True, default=False, help="read input file as json array"
+)
+@click.option("-s", "--sep", "separator", type=separator_type, default=".", help="separator")
+@click.option("--safe", is_flag=True, help="use safe mode")
+@click.option("-r", "--restore", "restore", is_flag=True, help="restore expanded json")
+@click.option("-e", "--expand", "expand", is_flag=True, help="expand json (default True)")
+@click.argument("input", type=click.File("r", encoding="utf-8"), default="-")
+@click.argument("output", type=click.File("w", encoding="utf-8"), default="-")
 def jsoncsv(
     output: io.TextIOBase,
     input: io.TextIOBase,
@@ -59,42 +41,34 @@ def jsoncsv(
     json_array: bool,
 ) -> None:
     if expand and restore:
-        raise click.UsageError('can not choose both, default is `-e`')
+        raise click.UsageError("can not choose both, default is `-e`")
 
     func: Callable[..., Any]
     func = expand_fn if not restore else restore_fn
 
-    convert_json(input,
-                 output,
-                 func,
-                 separator=separator,
-                 safe=safe,
-                 json_array=json_array)
+    convert_json(input, output, func, separator=separator, safe=safe, json_array=json_array)
 
     input.close()
     output.close()
 
 
 @click.command()
-@click.option('-t',
-              '--type',
-              'type_',
-              type=click.Choice(['csv', 'xls']),
-              default='csv',
-              help='choose dump format')
-@click.option('-r',
-              '--row',
-              type=int,
-              default=None,
-              help='number of pre-read `row` lines to load `headers`')
-@click.option('-s',
-              '--sort',
-              'sort_',
-              is_flag=True,
-              default=False,
-              help='enable sort the headers keys')
-@click.argument('input', type=click.File('r', encoding='utf-8'), default='-')
-@click.argument('output', type=click.Path(), default='-')
+@click.option(
+    "-t",
+    "--type",
+    "type_",
+    type=click.Choice(["csv", "xls"]),
+    default="csv",
+    help="choose dump format",
+)
+@click.option(
+    "-r", "--row", type=int, default=None, help="number of pre-read `row` lines to load `headers`"
+)
+@click.option(
+    "-s", "--sort", "sort_", is_flag=True, default=False, help="enable sort the headers keys"
+)
+@click.argument("input", type=click.File("r", encoding="utf-8"), default="-")
+@click.argument("output", type=click.Path(), default="-")
 def mkexcel(
     output: str,
     input: io.TextIOBase,
@@ -107,13 +81,13 @@ def mkexcel(
         klass = dumptool.DumpXLS
 
     # Open file in appropriate mode based on type
-    if output == '-':
-        fout: Any = sys.stdout.buffer if type_ == 'xls' else sys.stdout
+    if output == "-":
+        fout: Any = sys.stdout.buffer if type_ == "xls" else sys.stdout
         dump_excel(input, fout, klass, read_row=row, sort_type=sort_)
     else:
-        mode = 'wb' if type_ == 'xls' else 'w'
-        encoding = None if type_ == 'xls' else 'utf-8'
-        newline = '' if type_ == 'csv' else None
+        mode = "wb" if type_ == "xls" else "w"
+        encoding = None if type_ == "xls" else "utf-8"
+        newline = "" if type_ == "csv" else None
         with open(output, mode, encoding=encoding, newline=newline) as fout:
             dump_excel(input, fout, klass, read_row=row, sort_type=sort_)
 
--- tests/test_dumptool.py
+++ tests/test_dumptool.py
@@ -7,39 +7,54 @@ from jsoncsv.dumptool import DumpCSV, DumpXLS, dump_excel
 
 
 class TestDumpTool(unittest.TestCase):
-
     # FIXME (使用虚拟文件)
     def test_dumpexcel_csv(self):
-        with open('./fixture/files/expand.1.json', encoding='utf-8') as fin, \
-                open('./fixture/files/tmp.output.1.csv', 'w', encoding='utf-8', newline='') as fout:
+        with (
+            open("./fixture/files/expand.1.json", encoding="utf-8") as fin,
+            open("./fixture/files/tmp.output.1.csv", "w", encoding="utf-8", newline="") as fout,
+        ):
             dump_excel(fin, fout, DumpCSV)
 
-        with open('./fixture/files/output.1.csv', encoding='utf-8') as output, \
-                open('./fixture/files/tmp.output.1.csv', encoding='utf-8') as fout:
+        with (
+            open("./fixture/files/output.1.csv", encoding="utf-8") as output,
+            open("./fixture/files/tmp.output.1.csv", encoding="utf-8") as fout,
+        ):
             self.assertEqual(output.read(), fout.read())
 
     def test_dumpexcel_csv_with_sort(self):
-        with open('./fixture/files/expand.1.json', encoding='utf-8') as fin, \
-                open('./fixture/files/tmp.output.1.sort.csv', 'w', encoding='utf-8', newline='') as fout:
+        with (
+            open("./fixture/files/expand.1.json", encoding="utf-8") as fin,
+            open(
+                "./fixture/files/tmp.output.1.sort.csv", "w", encoding="utf-8", newline=""
+            ) as fout,
+        ):
             dump_excel(fin, fout, DumpCSV, sort_type=True)
 
-        with open('./fixture/files/output.1.sort.csv', encoding='utf-8') as output, \
-                open('./fixture/files/tmp.output.1.sort.csv', encoding='utf-8') as fout:
+        with (
+            open("./fixture/files/output.1.sort.csv", encoding="utf-8") as output,
+            open("./fixture/files/tmp.output.1.sort.csv", encoding="utf-8") as fout,
+        ):
             self.assertEqual(output.read(), fout.read())
 
     def test_dumpcexcel_xls(self):
-        with open('./fixture/files/expand.1.json', encoding='utf-8') as fin, \
-                open('./fixture/files/tmp.output.1.xls', 'wb') as fout:
+        with (
+            open("./fixture/files/expand.1.json", encoding="utf-8") as fin,
+            open("./fixture/files/tmp.output.1.xls", "wb") as fout,
+        ):
             dump_excel(fin, fout, DumpXLS)
 
     def test_dump_csv_with_non_ascii(self):
-        with open('./fixture/files/expand.2.json', encoding='utf-8') as fin, \
-                open('./fixture/files/tmp.output.2.csv', 'w', encoding='utf-8', newline='') as fout:
+        with (
+            open("./fixture/files/expand.2.json", encoding="utf-8") as fin,
+            open("./fixture/files/tmp.output.2.csv", "w", encoding="utf-8", newline="") as fout,
+        ):
             dump_excel(fin, fout, DumpCSV)
 
     def test_dump_xls_with_non_ascii(self):
-        with open('./fixture/files/expand.2.json', encoding='utf-8') as fin, \
-                open('./fixture/files/tmp.output.2.xls', 'wb') as fout:
+        with (
+            open("./fixture/files/expand.2.json", encoding="utf-8") as fin,
+            open("./fixture/files/tmp.output.2.xls", "wb") as fout,
+        ):
             dump_excel(fin, fout, DumpXLS)
 
     def test_dump_xls_with_dict(self):
